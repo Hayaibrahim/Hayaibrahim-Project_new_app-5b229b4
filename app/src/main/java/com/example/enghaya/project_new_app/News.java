4d68a9b2a2b9956a5b7fb67b0f5b5f7d4b1c3b42
ENG.HAYA <haya05055665959>
2017-10-07 12:12:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/enghaya/project_new_app/News.java
@@ -0,0 +1,50 @@
+package com.example.enghaya.project_new_app;
+
+import android.text.TextUtils;
+
+/**
+ * Created by ENG.HAYA on 10/2/2017 AD.
+ */
+
+public class News {
+    private String title;
+    private String url;
+    private String section;
+    private String date;
+    private String athors;
+
+
+    public News(String title, String section, String url, String data) {
+        this.title = title;
+        this.url = url;
+        this.section = section;
+        this.date = data;
+        this.athors = athors;
+    }
+
+    public News() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getAthors() {
+        return athors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getSection() {
+        if (TextUtils.isEmpty(section)) {
+            return "No section";
+        }
+        return section;
+    }
+}
